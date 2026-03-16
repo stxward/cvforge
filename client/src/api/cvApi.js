3d3ed6be00a788e3416aa857b6c1a83fd9e4ae35
Steward Jacob <Stxwardspam@gmail.com>
2026-03-16 12:49:10 +0530

    Hardcode production API URL

--- client/src/api/cvApi.js
+++ client/src/api/cvApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://cvforge-server.onrender.com/api';
 
 export const uploadCV = async (file) => {
   const formData = new FormData();
